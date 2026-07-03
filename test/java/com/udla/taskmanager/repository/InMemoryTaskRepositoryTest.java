2a68fad2abae426510b9b9bb9ce8bf95c3b45671
G4B0CM <gabo1302cm@gmail.com}>
2026-07-02 20:08:50 -0500

    fix: configurar testSourceDirectory y agregar contract test de   repositorio

--- test/java/com/udla/taskmanager/repository/InMemoryTaskRepositoryTest.java
+++ test/java/com/udla/taskmanager/repository/InMemoryTaskRepositoryTest.java
@@ -1,3 +1,14 @@
-public class InMemoryTaskRepositoryTest {
-    
+package com.udla.taskmanager.repository;
+
+/**
+ * Verifica que InMemoryTaskRepository cumple el contrato de
+ * TaskRepository ejecutando la suite heredada de
+ * TaskRepositoryContractTest.
+ */
+class InMemoryTaskRepositoryTest extends TaskRepositoryContractTest {
+
+    @Override
+    protected TaskRepository createRepository() {
+        return new InMemoryTaskRepository();
+    }
 }
